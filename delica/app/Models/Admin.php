66985942a3befb8223f9a3a60968013103fde610
thisalma <alwisthisalma@gamil/.com>
2025-12-27 16:13:33 +0530

    making the admin model

--- delica/app/Models/Admin.php
+++ delica/app/Models/Admin.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password'];
 }
+
